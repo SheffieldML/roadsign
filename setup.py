7990b0aeaa8b603b04f350619a5856b59fee84f7
Mike Smith <m.t.smith@sheffield.ac.uk>
2025-07-04 17:58:15 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'roadsign',
+  packages = ['roadsign'],
+  version = '0.0.1',
+  description = 'Open Day roadsign activity',
+  author = 'Mike Smith',
+  author_email = 'm.t.smith@sheffield.ac.uk',
+  url = 'https://github.com/SheffieldML/roadsign.git',
+  download_url = 'https://github.com/SheffieldML/roadsign.git',
+  keywords = ['open day','adversarial examples','road signs'],
+  classifiers = [],
+  install_requires=['yolov5==7.0.0','opencv-python==4.7.0.68'],
+  scripts=['bin/openday']
+)
+
