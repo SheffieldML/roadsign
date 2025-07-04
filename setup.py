45ff0182844899f189a08788d4e3897d438cfd2f
Mike Smith <m.t.smith@sheffield.ac.uk>
2025-07-04 19:05:15 +0100

    fixing missing file

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
   url = 'https://github.com/SheffieldML/roadsign.git',
   download_url = 'https://github.com/SheffieldML/roadsign.git',
   keywords = ['open day','adversarial examples','road signs'],
+  include_package_data=False,
+  package_data={ "": ["bin/best.pt"] }
   classifiers = [],
   install_requires=['yolov5 @ git+https://github.com/SheffieldML/yolov5-pip.git','opencv-python==4.7.0.68','numpy<2'],
   scripts=['bin/openday']
